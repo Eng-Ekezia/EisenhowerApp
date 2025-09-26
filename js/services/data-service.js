7c00255c90023241a5a25c5831d7707e6b53be7e
Eng-Ekezia <delima.eletrica@gmail.com>
2025-09-26 11:39:43 -0300

    feat(service): create data-service module for data management

---
+++ js/services/data-service.js
@@ -0,0 +1,8 @@
+// js/services/data-service.js
+
+/**
+ * Serviço para lidar com operações de importação e exportação de dados.
+ */
+export const dataService = {
+    // As funções de exportar e importar serão adicionadas aqui nos próximos passos.
+};
